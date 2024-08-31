e14c960f27a27ed201acb03c9bde63afba48ef76
Vaibhav768 <vaibhavbagal43@gmail.com>
2024-08-31 20:11:56 +0530

    dev1 index.jsp modified

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,10 +9,16 @@ public class App
     public String welcome(){
         System.out.println("Welcome to GitHub.com")
     }
+    public String greet(){
+        System.out.println("Grettings from Vaibhav..");
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         App app = new App();
         System.out.println(app.welcome(););
+        String msg = app.greet();
+        System.out.println(msg);
     }
+
 }
