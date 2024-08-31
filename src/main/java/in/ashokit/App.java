1bf3bd4e671f449d2319a3bebde899f9c3bfa4d8
Vaibhav768 <vaibhavbagal43@gmail.com>
2024-08-31 18:20:46 +0530

    dev 02 modifications

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -6,8 +6,13 @@ package in.ashokit;
  */
 public class App 
 {
+    public String welcome(){
+        System.out.println("Welcome to GitHub.com")
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App app = new App();
+        System.out.println(app.welcome(););
     }
 }
